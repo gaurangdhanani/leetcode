c85a0a9c1213e95919857e296bd27e215f8bcea3
Gaurang <66173531+gaurangdhanani@users.noreply.github.com>
2024-03-11 12:32:04 -0500

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            int x = target - nums[i];
+            int index = linearSearch(nums, x, i + 1);
+            if (index != -1) {
+                result.push_back(i);
+                result.push_back(index);
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+private:
+    int linearSearch(const vector<int>& nums, int target, int start) {
+        for (int i = start; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Below Code is binary but does not work for every testcases.
+/*private:
+    int binarySearch(const std::vector<int>& nums, int target, int start) {
+        int left = start;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};*/
