aa144fef88697adf6f0c9d38b96628d89c72337c
Gaurang <66173531+gaurangdhanani@users.noreply.github.com>
2024-08-05 15:38:30 -0500

    21.java

---
+++ 21.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (list1 != null) {
+            list.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while (list2 != null) {
+            list.add(list2.val);
+            list2 = list2.next;
+        }
+
+        Collections.sort(list);
+
+        ListNode temp = new ListNode(-1);
+        ListNode head = temp;
+        for (int val : list) {
+            temp.next = new ListNode(val);
+            temp = temp.next;
+        }
+        head = head.next;
+
+        return head;
+    }
+}
